d6b2691bf5f7f99bfcadd1ffed57972fa0adafd5
Qweeqwqwe123 <42992961+Qweeqwqwe123@users.noreply.github.com>
2018-12-31 17:19:30 +0200

    Add files via upload

--- 1B2.2.py
+++ 1B2.2.py
@@ -2,9 +2,8 @@ import sys
 sys.path.append('/user/local/anaconda3/lib/python3.6/site-packages')
 
 from numpy import *
-x = linspace(0, 7, 70)
-y = cos(x)
-y2 = sin(x)
+x = linspace(0, 7)
+y = sin(x)
 
 from matplotlib import pyplot as plt
 plt.grid()
@@ -12,5 +11,4 @@ plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.title('Funkcija $cos(x)$')
 plt.plot(x, y)
-plt.plot(x, y2)
 plt.show()
